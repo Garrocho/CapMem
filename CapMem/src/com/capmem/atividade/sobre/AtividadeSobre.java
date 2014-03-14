6dd02b9355588c825a6dbcadf86a2cbde39b97b1
CharlesGarrocho <charles.garrocho@gmail.com>
2014-03-14 11:17:08 -0300

    removido: comentário desnecessário. #5

--- CapMem/src/com/capmem/atividade/sobre/AtividadeSobre.java
+++ CapMem/src/com/capmem/atividade/sobre/AtividadeSobre.java
@@ -16,7 +16,6 @@ public class AtividadeSobre extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.atividade_sobre, menu);
 		return true;
 	}
